35bcacaf52a2f3e04eeec5aa0fcb6259eeb50d6a
HiImMars <fedorenkorostyslav@gmail.com>
2023-10-06 01:01:34 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,78 @@
 import { Component } from 'react';
-import { ContactForm } from './Phonebook.jsx/ContactForm';
-import { ContactList } from './Phonebook.jsx/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix';
 
 export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  createContact = data => {
+    const { contacts } = this.state;
+    if (contacts.some(contact => contact.name === data.name)) {
+      return Notify.info(`${data.name} is already in your contacts`);
+    }
+
+    const newContact = {
+      ...data,
+      id: nanoid(),
+    };
+
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, newContact],
+      };
+    });
+    Notify.success(`${data.name} has been successfully added to your contacts`);
+  };
+
+  onRemoveContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+    Notify.success('The contact has been successfully removed');
+  };
+
+  onFilterChange = evt => {
+    this.setState({ filter: evt.currentTarget.value });
+  };
+
+  handleFilterContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   render() {
+    const { filter, contacts } = this.state;
+
+    const filteredContacts = this.handleFilterContacts();
+
     return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <h2>Contacts</h2>
-        <ContactList />
+      <div className="container">
+        <h1 className="title">Phonebook</h1>
+        <ContactForm createContact={this.createContact} />
+        <h2 className="title">Contacts</h2>
+        <Filter value={filter} onChange={this.onFilterChange} />
+        {contacts.length ? (
+          <ContactList
+            contacts={filteredContacts}
+            onRemoveContact={this.onRemoveContact}
+          />
+        ) : (
+          <p className="">There are no contacts in your phoneboook</p>
+        )}
       </div>
     );
   }
