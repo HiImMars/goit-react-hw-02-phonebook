dd7572597268112aae58e0a0f732dec0ff1482b1
HiImMars <fedorenkorostyslav@gmail.com>
2023-10-11 21:57:48 +0300

    made hw 3 first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,22 @@ import { Notify } from 'notiflix';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts.length < this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+    if (prevState.contacts.length > this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   createContact = data => {
     const { contacts } = this.state;
@@ -48,6 +56,7 @@ export class App extends Component {
 
   handleFilterContacts = () => {
     const { contacts, filter } = this.state;
+
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
